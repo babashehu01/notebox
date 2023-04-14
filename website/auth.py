e4205fd9e85854e1e0bcec81deeef934ce39d553
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-04-14 17:23:20 +0100

    Adding my auth blueprint

--- website/auth.py
+++ website/auth.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
+
 auth = Blueprint('auth', __name__)
 
 
@@ -26,6 +27,7 @@ def login():
 
     return render_template("login.html", user=current_user)
 
+
 @auth.route('/logout')
 @login_required
 def logout():
@@ -38,8 +40,8 @@ def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
-        password = request.form.get('password')
-        confirm_password = request.form.get('confirm_password')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
         user = User.query.filter_by(email=email).first()
         if user:
@@ -48,15 +50,18 @@ def sign_up():
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
-        elif password != confirm_password:
+        elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-            password, method='sha256'))
+                password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
+
